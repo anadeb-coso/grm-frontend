119e21f26efd3a59d3f48803761a38e1a92a67b4
Freddy F <freddyrp16@gmail.com>
2022-03-22 23:36:28 -0500

    Fixed android and iOS dropdown picker overlapping

--- src/components/CustomDropDownPicker/CustomDropDownPicker.js
+++ src/components/CustomDropDownPicker/CustomDropDownPicker.js
@@ -1,43 +1,47 @@
-import React, { useState } from "react";
-import { styles } from "./CustomDropDownPicker.style";
-import DropDownPicker from "react-native-dropdown-picker";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { colors } from "../../utils/colors";
-import { Dimensions, View } from "react-native";
+import React, { useState } from 'react';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Dimensions, Platform, View } from 'react-native';
+import { colors } from '../../utils/colors';
+import { styles } from './CustomDropDownPicker.style';
 
-const screenHeight = Dimensions.get("window").height;
+const screenHeight = Dimensions.get('window').height;
 
-const CustomDropDownPicker = ({
+function CustomDropDownPicker({
   value,
   items,
-  listMode="SCROLLVIEW",
-  scrollViewProps={
-    nestedScrollEnabled: true
+  listMode = 'SCROLLVIEW',
+  scrollViewProps = {
+    nestedScrollEnabled: true,
   },
   setPickerValue,
   setItems,
   placeholder,
   onChangeValue,
   schema,
-  zIndex=100,
+  zIndex = 100,
+  zIndexInverse = 100,
   customDropdownWrapperStyle,
   disabled = false,
   onOpen = () => null,
   onClose = () => null,
   onSelectItem = () => null,
-}) => {
+}) {
   const [dropdownVisible, setDropdownVisible] = React.useState(false);
   const [open, setOpen] = useState(false);
   return (
     <View
       style={[
         styles.dropdownWrapper,
-        { minHeight: screenHeight * (dropdownVisible ? 0.40 : 0), zIndex },
+        { minHeight: screenHeight * (dropdownVisible ? 0.4 : 0), zIndex },
         customDropdownWrapperStyle,
+        Platform.OS === 'ios' ? { zIndex: dropdownVisible ? 1 : 0 } : {},
+        Platform.OS === 'android' ? { elevation: dropdownVisible ? 10 : 0 } : {},
       ]}
     >
       <DropDownPicker
-          zIndex={zIndex}
+        zIndex={zIndex}
+        zIndexInverse={zIndexInverse}
         schema={schema}
         disabled={disabled}
         onOpen={() => {
@@ -49,28 +53,20 @@ const CustomDropDownPicker = ({
           setDropdownVisible(false);
         }}
         ArrowUpIconComponent={({ style }) => (
-          <MaterialCommunityIcons
-            name="chevron-up-circle"
-            size={24}
-            color={colors.primary}
-          />
+          <MaterialCommunityIcons name="chevron-up-circle" size={24} color={colors.primary} />
         )}
         ArrowDownIconComponent={({ style }) => (
-          <MaterialCommunityIcons
-            name="chevron-down-circle"
-            size={24}
-            color={colors.primary}
-          />
+          <MaterialCommunityIcons name="chevron-down-circle" size={24} color={colors.primary} />
         )}
-          onSelectItem={(item) => onSelectItem(item)}
+        onSelectItem={(item) => onSelectItem(item)}
         style={styles.dropdownStyle}
         dropDownContainerStyle={styles.dropdownContainer}
         textStyle={styles.dropdownText}
         labelStyle={styles.dropdownLabel}
-        itemSeparator={true}
+        itemSeparator
         onChangeValue={onChangeValue}
         itemSeparatorStyle={{
-          backgroundColor: "#f6f6f6",
+          backgroundColor: '#f6f6f6',
         }}
         placeholder={placeholder}
         open={open}
@@ -84,6 +80,6 @@ const CustomDropDownPicker = ({
       />
     </View>
   );
-};
+}
 
 export default CustomDropDownPicker;
--- src/screens/Home/CitizenReportContactInfo/containers/Content.js
+++ src/screens/Home/CitizenReportContactInfo/containers/Content.js
@@ -1,31 +1,25 @@
-import React, {useEffect, useState} from "react";
-import {
-  View,
-  ScrollView,
-  Text,
-  KeyboardAvoidingView,
-  Platform,
-} from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { styles } from "./Content.styles";
-import {Button, TextInput, Checkbox, RadioButton} from "react-native-paper";
-import { colors } from "../../../../utils/colors";
-import i18n from "i18n-js";
-import CustomDropDownPicker from "../../../../components/CustomDropDownPicker/CustomDropDownPicker";
+import React, { useEffect, useState } from 'react';
+import { View, ScrollView, Text, KeyboardAvoidingView, Platform } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Button, TextInput, Checkbox, RadioButton } from 'react-native-paper';
+import i18n from 'i18n-js';
+import { styles } from './Content.styles';
+import { colors } from '../../../../utils/colors';
+import CustomDropDownPicker from '../../../../components/CustomDropDownPicker/CustomDropDownPicker';
 
 const theme = {
   roundness: 12,
   colors: {
     ...colors,
-    background: "white",
-    placeholder: "#dedede",
-    text: "#707070",
+    background: 'white',
+    placeholder: '#dedede',
+    text: '#707070',
   },
 };
 
 function Content({ stepOneParams, issueAges, citizenGroupsI, citizenGroupsII }) {
   const navigation = useNavigation();
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
   const [checked, setChecked] = useState(false);
   const [contactMethodError, setContactMethodError] = React.useState();
   const [isPreviousPickerClosed, setIsPreviousPickerClosed] = useState(true);
@@ -39,10 +33,10 @@ function Content({ stepOneParams, issueAges, citizenGroupsI, citizenGroupsII }) 
   const [ages, setAges] = useState(issueAges ?? []);
   const [pickerGenderValue, setPickerGenderValue] = useState(null);
   const [genders, setGenders] = useState([
-    { label: i18n.t("male"), value: "male" },
-    { label: i18n.t("female"), value: "female" },
-    { label: "Other", value: "other" },
-    { label: "Rather not say", value: "rather_not_say" },
+    { label: i18n.t('male'), value: 'male' },
+    { label: i18n.t('female'), value: 'female' },
+    { label: 'Other', value: 'other' },
+    { label: 'Rather not say', value: 'rather_not_say' },
   ]);
 
   useEffect(() => {
@@ -55,31 +49,24 @@ function Content({ stepOneParams, issueAges, citizenGroupsI, citizenGroupsII }) 
     if (issueAges) {
       setAges(issueAges);
     }
-
   }, [citizenGroupsI, citizenGroupsII]);
 
   return (
     <ScrollView>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "position" : null}
-      >
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'position' : null}>
         <View style={{ padding: 23 }}>
-          <Text style={styles.stepText}>{i18n.t("step_2")}</Text>
-          <Text style={styles.stepDescription}>
-            {i18n.t("contact_step_subtitle")}
-          </Text>
-          <Text style={styles.stepNote}>
-            {i18n.t("contact_step_explanation")}
-          </Text>
+          <Text style={styles.stepText}>{i18n.t('step_2')}</Text>
+          <Text style={styles.stepDescription}>{i18n.t('contact_step_subtitle')}</Text>
+          <Text style={styles.stepNote}>{i18n.t('contact_step_explanation')}</Text>
         </View>
 
         <View style={{ paddingHorizontal: 50 }}>
           <TextInput
             style={styles.grmInput}
-            placeholder={i18n.t("contact_step_placeholder_1")}
-            outlineColor={"#f6f6f6"}
+            placeholder={i18n.t('contact_step_placeholder_1')}
+            outlineColor="#f6f6f6"
             theme={theme}
-            mode={"outlined"}
+            mode="outlined"
             value={name}
             error={contactMethodError}
             onChangeText={(text) => {
@@ -89,50 +76,45 @@ function Content({ stepOneParams, issueAges, citizenGroupsI, citizenGroupsII }) 
           />
           <Text />
           <RadioButton.Group
-              onValueChange={(newValue) => {
-                if(newValue == confidentialValue){
-                  setConfidentialValue(0)
-                } else {
-                  setConfidentialValue(newValue)
-                }
-              }}
-              value={confidentialValue}
+            onValueChange={(newValue) => {
+              if (newValue === confidentialValue) {
+                setConfidentialValue(0);
+              } else {
+                setConfidentialValue(newValue);
+              }
+            }}
+            value={confidentialValue}
           >
-            <View style={{ flexDirection: "row", alignItems: "center", marginVertical: 5 }}>
-              <RadioButton.Android
-                  value={1}
-                  uncheckedColor={"#dedede"}
-                  color={colors.primary}
-              />
-              <Text style={styles.radioLabel}>Keep name confidential.  Only the person resolving the issue will see the name. </Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 5 }}>
+              <RadioButton.Android value={1} uncheckedColor="#dedede" color={colors.primary} />
+              <Text style={styles.radioLabel}>
+                Keep name confidential. Only the person resolving the issue will see the name.{' '}
+              </Text>
             </View>
-            <View style={{ flexDirection: "row", alignItems: "center", marginVertical: 5 }}>
-              <RadioButton.Android
-                  value={2}
-                  uncheckedColor={"#dedede"}
-                  color={colors.primary}
-              />
-              <Text style={styles.radioLabel}>This is an individual filing on behalf of someone else. </Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 5 }}>
+              <RadioButton.Android value={2} uncheckedColor="#dedede" color={colors.primary} />
+              <Text style={styles.radioLabel}>
+                This is an individual filing on behalf of someone else.{' '}
+              </Text>
             </View>
-            <View style={{ flexDirection: "row", alignItems: "center", marginVertical: 5 }}>
-              <RadioButton.Android
-                  value={3}
-                  uncheckedColor={"#dedede"}
-                  color={colors.primary}
-              />
-              <Text style={styles.radioLabel}>This is an organization filing on behalf of someone else.</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 5 }}>
+              <RadioButton.Android value={3} uncheckedColor="#dedede" color={colors.primary} />
+              <Text style={styles.radioLabel}>
+                This is an organization filing on behalf of someone else.
+              </Text>
             </View>
           </RadioButton.Group>
-
         </View>
         <Text />
         <CustomDropDownPicker
-            schema={{
-              label: "name",
-              value: "id",
-            }}
-            onSelectItem={(item)=> setSelectedAge(item)}
-          placeholder={i18n.t("contact_step_placeholder_2")}
+          schema={{
+            label: 'name',
+            value: 'id',
+          }}
+          zIndex={4000}
+          zIndexInverse={1000}
+          onSelectItem={(item) => setSelectedAge(item)}
+          placeholder={i18n.t('contact_step_placeholder_2')}
           value={pickerAgeValue}
           onOpen={() => setIsPreviousPickerClosed(false)}
           onClose={() => setIsPreviousPickerClosed(true)}
@@ -143,43 +125,49 @@ function Content({ stepOneParams, issueAges, citizenGroupsI, citizenGroupsII }) 
         {isPreviousPickerClosed && (
           <>
             <CustomDropDownPicker
-              placeholder={i18n.t("contact_step_placeholder_3")}
+              placeholder={i18n.t('contact_step_placeholder_3')}
               value={pickerGenderValue}
               items={genders}
+              zIndex={3000}
+              zIndexInverse={2000}
               setPickerValue={setPickerGenderValue}
               setItems={setGenders}
             />
             <CustomDropDownPicker
-                schema={{
-                  label: "name",
-                  value: "id",
-                }}
-                placeholder={'Citizen Group I'}
-                value={selectedCitizenGroupI}
-                items={_citizenGroupsI}
-                setPickerValue={setSelectedCitizenGroupI}
-                setItems={setCitizenGroupsI}
+              schema={{
+                label: 'name',
+                value: 'id',
+              }}
+              zIndex={2000}
+              zIndexInverse={3000}
+              placeholder="Citizen Group I"
+              value={selectedCitizenGroupI}
+              items={_citizenGroupsI}
+              setPickerValue={setSelectedCitizenGroupI}
+              setItems={setCitizenGroupsI}
             />
             <CustomDropDownPicker
-                schema={{
-                  label: "name",
-                  value: "id",
-                }}
-                placeholder={'Citizen Group II'}
-                value={selectedCitizenGroupII}
-                items={_citizenGroupsII}
-                setPickerValue={setSelectedCitizenGroupII}
-                setItems={setCitizenGroupsII}
+              schema={{
+                label: 'name',
+                value: 'id',
+              }}
+              placeholder="Citizen Group II"
+              value={selectedCitizenGroupII}
+              zIndex={1000}
+              zIndexInverse={4000}
+              items={_citizenGroupsII}
+              setPickerValue={setSelectedCitizenGroupII}
+              setItems={setCitizenGroupsII}
             />
             <View style={{ paddingHorizontal: 50 }}>
               <Button
                 theme={theme}
-                style={{ alignSelf: "center", margin: 24 }}
-                labelStyle={{ color: "white", fontFamily: "Poppins_500Medium" }}
+                style={{ alignSelf: 'center', margin: 24 }}
+                labelStyle={{ color: 'white', fontFamily: 'Poppins_500Medium' }}
                 mode="contained"
                 onPress={() => {
                   if (name) {
-                    navigation.navigate("CitizenReportStep2", {
+                    navigation.navigate('CitizenReportStep2', {
                       stepOneParams: {
                         ...stepOneParams,
                         name,
@@ -192,13 +180,11 @@ function Content({ stepOneParams, issueAges, citizenGroupsI, citizenGroupsII }) 
                       },
                     });
                   } else {
-                    setContactMethodError(
-                      "Please insert a valid method of contact"
-                    );
+                    setContactMethodError('Please insert a valid method of contact');
                   }
                 }}
               >
-                {i18n.t("next")}
+                {i18n.t('next')}
               </Button>
             </View>
           </>
