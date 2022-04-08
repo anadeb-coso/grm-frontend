d2a1ecd310d5e099c1faab1e171cabdc3f95f773
Freddy F <freddyrp16@gmail.com>
2022-04-08 14:40:24 -0500

    Fixed multiple files upload

--- src/screens/Auth/Login/Login.js
+++ src/screens/Auth/Login/Login.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import styles from "./Login.style";
 import {
   Keyboard,
   Text,
@@ -9,19 +8,19 @@ import {
   KeyboardAvoidingView,
   TouchableOpacity,
   ScrollView,
-} from "react-native";
-import {ActivityIndicator, Button, Title} from "react-native-paper";
-import { useDispatch } from "react-redux";
-import { login } from "../../../store/ducks/authentication.duck";
-import MapBg from "../../../../assets/map-bg.svg";
-import EADLLogo from "../../../../assets/eadl-logo.svg";
-import { TextInput } from "react-native-paper";
-import { Controller, useForm } from "react-hook-form";
-import MESSAGES from "../../../utils/formErrorMessages";
-import { emailRegex, passwordRegex } from "../../../utils/formUtils";
-import API from "../../../services/API";
-import { getEncryptedData } from "../../../utils/storageManager";
-import {titles} from "../../Onboarding/containers/Content/utils";
+} from 'react-native';
+import { ActivityIndicator, Button, Title, TextInput } from 'react-native-paper';
+import { useDispatch } from 'react-redux';
+import { Controller, useForm } from 'react-hook-form';
+import { login } from '../../../store/ducks/authentication.duck';
+import MapBg from '../../../../assets/map-bg.svg';
+import EADLLogo from '../../../../assets/eadl-logo.svg';
+import styles from './Login.style';
+import MESSAGES from '../../../utils/formErrorMessages';
+import { emailRegex, passwordRegex } from '../../../utils/formUtils';
+import API from '../../../services/API';
+import { getEncryptedData } from '../../../utils/storageManager';
+import { titles } from '../../Onboarding/containers/Content/utils';
 
 function Login() {
   const dispatch = useDispatch();
@@ -31,12 +30,10 @@ function Login() {
   const onLoginPress = async (data) => {
     setLoading(true);
     const dbConfig = await getEncryptedData(
-      `dbCredentials_${data?.password}_${data?.email.replace("@", "")}`
+      `dbCredentials_${data?.password}_${data?.email.replace('@', '')}`
     );
     if (dbConfig) {
-      dispatch(
-        login(dbConfig, { email: data?.email, password: data?.password })
-      );
+      dispatch(login(dbConfig, { email: data?.email, password: data?.password }));
     } else {
       new API()
         .login({ email: data?.email, password: data?.password })
@@ -55,13 +52,13 @@ function Login() {
   };
 
   const { control, handleSubmit, errors } = useForm({
-    criteriaMode: "all",
+    criteriaMode: 'all',
   });
 
   return (
     <ScrollView
       style={{
-        backgroundColor: "white",
+        backgroundColor: 'white',
         flex: 1,
         paddingBottom: 30,
         paddingHorizontal: 30,
@@ -69,36 +66,37 @@ function Login() {
       contentContainerStyle={{ flexGrow: 1 }}
     >
       <KeyboardAvoidingView style={styles.containerView} behavior="position">
-        <View style={{ alignItems: "center", justifyContent: "center" }}>
-          {/*<MapBg*/}
-          {/*  width={220}*/}
-          {/*  height={190}*/}
-          {/*  style={{*/}
-          {/*    marginTop: -50,*/}
-          {/*  }}*/}
-          {/*/>*/}
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          {/* <MapBg */}
+          {/*  width={220} */}
+          {/*  height={190} */}
+          {/*  style={{ */}
+          {/*    marginTop: -50, */}
+          {/*  }} */}
+          {/* /> */}
         </View>
 
-        <View style={{ marginBottom: 50, marginTop: 70, alignItems: "center" }}>
-          {/*<EADLLogo height={90} width={180} />*/}
-          <Text style={{
-            marginBottom: 15,
-            fontFamily: "Poppins_400Regular",
-            fontSize: 19,
-            fontWeight: "bold",
-            fontStyle: "normal",
-            lineHeight: 23,
-            letterSpacing: 0,
-            textAlign: "center",
-            color: "#707070",
-          }}>Welcome!  Log-in here.</Text>
+        <View style={{ marginBottom: 50, marginTop: 70, alignItems: 'center' }}>
+          {/* <EADLLogo height={90} width={180} /> */}
+          <Text
+            style={{
+              marginBottom: 15,
+              fontFamily: 'Poppins_400Regular',
+              fontSize: 19,
+              fontWeight: 'bold',
+              fontStyle: 'normal',
+              lineHeight: 23,
+              letterSpacing: 0,
+              textAlign: 'center',
+              color: '#707070',
+            }}
+          >
+            Welcome! Log-in here.
+          </Text>
         </View>
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
           <View style={styles.loginScreenContainer}>
-            <KeyboardAvoidingView
-              style={styles.containerView}
-              behavior="padding"
-            >
+            <KeyboardAvoidingView style={styles.containerView} behavior="padding">
               <View style={styles.formContainer}>
                 <View style={{ borderRadius: 10, marginBottom: 16 }}>
                   <Controller
@@ -108,17 +106,16 @@ function Login() {
                         theme={{
                           roundness: 10,
                           colors: {
-                            primary: "#24c38b",
-                            placeholder: "#dedede",
+                            primary: '#24c38b',
+                            placeholder: '#dedede',
                           },
                         }}
-                        mode={"outlined"}
+                        autoCapitalize="none"
+                        mode="outlined"
                         label="Email"
                         labelColor="#dedede"
                         style={styles.loginFormTextInput}
-                        left={
-                          <TextInput.Icon name="account" color={"#24c38b"} />
-                        }
+                        left={<TextInput.Icon name="account" color="#24c38b" />}
                         onBlur={onBlur}
                         onChangeText={(value) => onChange(value)}
                         value={value}
@@ -132,14 +129,12 @@ function Login() {
                       },
                       pattern: {
                         value: emailRegex,
-                        message: "Please enter a valid email address",
+                        message: 'Please enter a valid email address',
                       },
                     }}
                     defaultValue=""
                   />
-                  {errors.email && (
-                    <Text style={styles.errorText}>{errors.email.message}</Text>
-                  )}
+                  {errors.email && <Text style={styles.errorText}>{errors.email.message}</Text>}
                   <Controller
                     control={control}
                     render={({ onChange, onBlur, value }) => (
@@ -147,19 +142,19 @@ function Login() {
                         theme={{
                           roundness: 10,
                           colors: {
-                            primary: "#24c38b",
-                            placeholder: "#dedede",
+                            primary: '#24c38b',
+                            placeholder: '#dedede',
                           },
                         }}
-                        mode={"outlined"}
+                        mode="outlined"
                         label="Password"
-                        placeholderColor={"#dedede"}
+                        placeholderColor="#dedede"
                         style={styles.loginFormTextInput}
                         left={
                           <TextInput.Icon
                             onPress={() => setIsPasswordSecure(!isPasswordSecure)}
-                            name={isPasswordSecure ? "eye-off-outline" : "eye-outline"}
-                            color={"#24c38b"}
+                            name={isPasswordSecure ? 'eye-off-outline' : 'eye-outline'}
+                            color="#24c38b"
                           />
                         }
                         value={value}
@@ -190,30 +185,28 @@ function Login() {
                     defaultValue=""
                   />
                   {errors.password && (
-                    <Text style={styles.errorText}>
-                      {errors.password.message}
-                    </Text>
+                    <Text style={styles.errorText}>{errors.password.message}</Text>
                   )}
                 </View>
 
-                {/*<TouchableOpacity style={styles.hintContainer}>*/}
-                {/*  <Text style={styles.textHint}>Forgo?</Text>*/}
-                {/*</TouchableOpacity>*/}
+                {/* <TouchableOpacity style={styles.hintContainer}> */}
+                {/*  <Text style={styles.textHint}>Forgo?</Text> */}
+                {/* </TouchableOpacity> */}
               </View>
             </KeyboardAvoidingView>
             {loading ? (
-              <ActivityIndicator color={"#24c38b"} />
+              <ActivityIndicator color="#24c38b" />
             ) : (
               <Button
                 style={[
                   styles.loginButton,
                   {
-                    backgroundColor: errors ? "#24c38b" : "#dedede",
-                    marginTop: "40%",
+                    backgroundColor: errors ? '#24c38b' : '#dedede',
+                    marginTop: '40%',
                   },
                 ]}
                 onPress={handleSubmit(onLoginPress)}
-                color={"white"}
+                color="white"
               >
                 LOGIN
               </Button>
