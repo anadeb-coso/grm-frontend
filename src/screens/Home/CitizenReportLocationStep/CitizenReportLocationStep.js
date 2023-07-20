529f7096d2525601b40a10d35ef677edf6385f1b
adaboubvincent <adaboubvincent@gmail.com>
2023-07-20 12:50:01 +0000

    Updated loading location

--- src/screens/Home/CitizenReportLocationStep/CitizenReportLocationStep.js
+++ src/screens/Home/CitizenReportLocationStep/CitizenReportLocationStep.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { SafeAreaView, Alert } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { ActivityIndicator } from 'react-native-paper';
+import { useTranslation } from 'react-i18next';
 import { setCommune, setDocument } from '../../../store/ducks/userDocument.duck';
 import { getUserDocs } from '../../../utils/databaseManager';
 import { styles } from './CitizenReportLocationStep.styles';
@@ -12,6 +13,7 @@ import API from '../../../services/API';
 import NetInfo from '@react-native-community/netinfo';
 
 function CitizenReportLocationStep({ route }) {
+  const { t } = useTranslation();
   const { params } = route;
   const dispatch = useDispatch();
   //   const [issueCommunes, setIssueCommunes] = useState();
@@ -21,11 +23,11 @@ function CitizenReportLocationStep({ route }) {
   const { userCommune } = useSelector((state) => state.get('userDocument').toObject());
   const [cantons, setCantons] = useState(null);
   const [villages, setVillages] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchUserCommune = async () => {
       if (!userCommune) {
-        console.log(username)
         const { userDoc, userCommune: usrC } = await getUserDocs(username);
         if (userDoc) {
           dispatch(setDocument(userDoc)); // Dispatch setDocument action
@@ -60,38 +62,53 @@ function CitizenReportLocationStep({ route }) {
 //     console.log('Is connected?', state.isConnected);
 // });
 
-
+  setLoading(true);
   new API().administrativeLevelsFilterByAdministrativeRegion(userCommune.administrative_id, {}).then((response) => {
     if (response.error) {
+      setCantons([]);
+      setVillages([]);
       Alert.alert('Warning', response?.error?.toString(), [{ text: 'OK' }], {
         cancelable: false,
       });
       return;
+    }else{
+      setCantons(response.cantons);
+      setVillages(response.villages);
     }
-    setCantons(response.cantons);
-    setVillages(response.villages);
+    
+    setLoading(false);
+    
   });
 
   NetInfo.fetch().then((state) => {
       if(!state.isConnected){
         setCantons([]);
         setVillages([]);
+        setLoading(false);
       }
   });
 
   setTimeout(function(){
-    console.log("ici"),
-    setCantons([]);
-    setVillages([]);
+    if(loading) {
+      Alert.alert('Warning', t('warning_message_location_not_load'), [{ text: 'OK' }], {
+        cancelable: false,
+      });
+      setLoading(false);
+    }
   }, 10000);
+
 
 
   }, []);
 
   const customStyles = styles();
-  if (cantons == null || villages == null){
+  if (loading){
     return <ActivityIndicator style={{ marginTop: 50 }} color={colors.primary} size="small" />;
   }else{
+    if(cantons == null && villages == null){
+      setCantons([]);
+      setVillages([]);
+    }
     return (
       <SafeAreaView style={customStyles.container}>
         <Content
